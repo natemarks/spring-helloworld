bd5a9e20527c1bc6a259f05542f47b02ff286887
Nate Marks <npmarks@gmail.com>
2018-08-18 16:12:18 -0400

    solved the problem running JAR - needed  the RESPONSE_STRING environment variable

---
+++ src/main/java/com/springex/helloworld/Config.java
@@ -0,0 +1,7 @@
+package com.springex.helloworld;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix="com.springex.helloworld")
+public class Config {
+}
